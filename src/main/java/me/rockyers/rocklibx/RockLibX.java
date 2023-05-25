e1763f374ddb8b67e233672943dfcc6acbc5d892
Rockyers <ethan@yocom.org>
2023-05-25 01:06:16 -0700

    Plugin setup

--- src/main/java/me/rockyers/rocklibx/RockLibX.java
+++ src/main/java/me/rockyers/rocklibx/RockLibX.java
@@ -1,7 +1,18 @@
 package me.rockyers.rocklibx;
 
+import org.bukkit.plugin.PluginLoadOrder;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.plugin.Description;
+import org.bukkit.plugin.java.annotation.plugin.LoadOn;
+import org.bukkit.plugin.java.annotation.plugin.Plugin;
+import org.bukkit.plugin.java.annotation.plugin.Website;
+import org.bukkit.plugin.java.annotation.plugin.author.Author;
 
+@Plugin(name = "RockLibX", version = "1.0.0")
+@Description(desc = "A general use library for Minecraft plugins")
+@LoadOn(loadOn = PluginLoadOrder.STARTUP)
+@Author(name = "rockyers")
+@Website(url = "https://github.com/Rockyers/RockLibX")
 public class RockLibX extends JavaPlugin {
 
     @Override
