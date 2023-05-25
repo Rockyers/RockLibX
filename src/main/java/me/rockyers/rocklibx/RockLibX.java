fb1e1052dbc79f75c7a033e5685844239f2cb6a9
Rockyers <ethan@yocom.org>
2023-05-25 01:14:54 -0700

    Basic Javadoc comment for main class, RockLibX.java

--- src/main/java/me/rockyers/rocklibx/RockLibX.java
+++ src/main/java/me/rockyers/rocklibx/RockLibX.java
@@ -8,6 +8,11 @@ import org.bukkit.plugin.java.annotation.plugin.Plugin;
 import org.bukkit.plugin.java.annotation.plugin.Website;
 import org.bukkit.plugin.java.annotation.plugin.author.Author;
 
+/**
+ * The main plugin class, where the plugin starts. Take a look at the <a href="https://github.com/Rockyers/RockLibX">Github</a>
+ * <p>
+ * Using <a href="https://www.spigotmc.org/wiki/spigot-plugin-yml-annotations/?noRedirect=1">Spigot Plugin.yml Annotations</a> to generate the plugin.yml.
+ */
 @Plugin(name = "RockLibX", version = "1.0.0")
 @Description(desc = "A general use library for Minecraft plugins")
 @LoadOn(loadOn = PluginLoadOrder.STARTUP)
